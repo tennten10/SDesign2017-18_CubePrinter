6e9295c7ae38da3f05e70123018808e40987f73c
Michael Tenuta <tennten10@users.noreply.github.com>
2018-03-30 22:44:00 -0400

    Add files via upload

---
+++ NANO1SIGANDMOT/ERRORS.h
@@ -0,0 +1,26 @@
+// ERRORS.h
+//
+
+#if !defined(ERRORS_h)
+#define ERRORS_h
+
+enum ERRORS
+{
+	eCOM,
+	eLCD,
+	eSD,
+	eMOTOR,
+	e32V,
+	e5V,
+	eLED,
+	eALT,
+	ePOS,
+	eBEGIN,
+	SENSOR1_ERROR,
+	SENSOR2_ERROR,
+	SENSOR3_ERROR,
+	SENSOR4_ERROR,
+	SENSOR5_ERROR
+};
+
+#endif /* ERRORS_h */
